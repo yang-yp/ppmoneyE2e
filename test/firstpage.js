629697e98c2f664b7339db9a188095929cf2be0b
yang-yp <yangyp8084@sina.com>
2018-07-03 23:06:10 +0800

    some case automation

--- test/firstpage.js
+++ test/firstpage.js
@@ -1,10 +1,11 @@
 /**
- * created by Yyp on 2018/6/9
- * case coverage:PP-1,PP-2,PP-3
- * Pre-step :registe a account
+ * created by Yyp on 2018/6/14
+ * case coverage:PP-4,PP-5,PP-6
+ * Pre-step :user have longin
  * Test flow:
- * 1,open che Ppmoney net
- * 2,input tel-number ,login seccuss
+ * 1,on my account page click the 首页 button
+ * 2,on the home page click the 进入我的账户
+ * 3,on the home page click the 去充值
  */
 var EC = protractor.ExpectedConditions;
 var BasePage =require('../test-framework/core/base-page.js');
@@ -18,8 +19,8 @@ describe(" a case of longin secess",function (){
     afterAll(function(done){
      done();
     });
-    //PP-1
-    it("user login",function(done){
+    //PP-4
+    it("goto home page",function(done){
         basePage.navigateToURL('https://www.ppmoney.com/');
         var loginLink = element.all(by.css('ul#topLoginState.site-nav-r.fr > li > a' )).first();
         var userName =element(by.id('Phone'));
@@ -28,7 +29,8 @@ describe(" a case of longin secess",function (){
         var logIn =element(by.id('sendLogin'));
         var loginOk = element(by.css('div.face-side > p.s-num' ));
         var longOut =element(by.css('ul#topLoginState.site-nav-r.fr > li.menu.fr > a'));
-
+        var firstPage =element(by.css('ul.nav-c.cf > li.item.item-01.active > a'));
+        var money =element(by.css('div.login-box-bd > div.user-data.cf > div.data-box.fl > label'));
         if (EC.visibilityOf(closePopup)){
             closePopup.click();
         };
@@ -37,44 +39,51 @@ describe(" a case of longin secess",function (){
         userName.sendKeys("13718281997");
         passWord.sendKeys("ppmoney123");
         logIn.click();
+        firstPage.click();
         //expect(loginOk.isDisplayed()).toBe(true);
-        expect(loginOk.valueOf('注册号：1077513746487'));
+        expect(money.valueOf('可用余额(元)'));
         longOut.click();
         done();
     });
-    //PP-2
-    it("user incorrect user",function(done){
+    //PP-5
+    it("go to my account",function(done){
   //      basePage.navigateToURL('https://www.ppmoney.com/');
         var loginLink = element.all(by.css('ul#topLoginState.site-nav-r.fr > li > a' )).first();
         var userName =element(by.id('Phone'));
         var passWord =element(by.id('Password'));
-        var closePopup = element(by.id('popbtn'));
         var logIn =element(by.id('sendLogin'));
-        //var loginOk = element(by.css('div.face-side > p.s-num' ));
-        var loginWar = element(by.css('form#formLogin.login-bd > div.warning'));
+        var loginOk = element(by.css('div.face-side > p.s-num' ));
+        var longOut =element(by.css('ul#topLoginState.site-nav-r.fr > li.menu.fr > a'));
+        var firstPage =element(by.css('ul.nav-c.cf > li.item.item-01.active > a'));
+        var goMyAccount =element(by.css('div#enLogin.login-box > div.login-box-bd >  a.pp-btn.btn-reg'));
  /*       if (EC.visibilityOf(closePopup)){
             closePopup.click();
         };
  */
         loginLink.click();
         expect(userName.isDisplayed()).toBe(true);
-        userName.sendKeys("13718281991");
+        userName.sendKeys("13718281997");
         passWord.sendKeys("ppmoney123");
         logIn.click();
-        //expect(loginOk.isDisplayed()).toBe(true);
-        expect(loginWar.valueOf('帐号密码不匹配'));
+        firstPage.click();
+        goMyAccount.click();
+        expect(loginOk.valueOf('注册号：1077513746487'));
+        longOut.click();
         done();
     });
-    //PP-3
-    it("user incorrect password",function(done){
-        //      basePage.navigateToURL('https://www.ppmoney.com/');
+    //PP-6
+    it("recharge",function(done){
+        basePage.navigateToURL('https://www.ppmoney.com/');
         var loginLink = element.all(by.css('ul#topLoginState.site-nav-r.fr > li > a' )).first();
         var userName =element(by.id('Phone'));
         var passWord =element(by.id('Password'));
-        var closePopup = element(by.id('popbtn'));
         var logIn =element(by.id('sendLogin'));
-        //var loginOk = element(by.css('div.face-side > p.s-num' ));
-        var loginWar = element(by.css('form#formLogin.login-bd > div.warning'));
+        var loginOk = element(by.css('div.face-side > p.s-num' ));
+        var firstPage =element(by.css('ul.nav-c.cf > li.item.item-01.active > a'));
+        //var recharge =element(by.css('div.login-box-bd > div.user-data.cf > div.fl > a'));
+        var recharge =element(by.partialLinkText('去充值>'));
+        var openAccount =element(by.id('btnRegAccount'));
+        //var goMyAccount =element(by.css('div#enLogin.login-box > div.login-box-bd >  a.pp-btn.btn-reg'));
         /*       if (EC.visibilityOf(closePopup)){
                    closePopup.click();
                };
@@ -82,10 +91,13 @@ describe(" a case of longin secess",function (){
         loginLink.click();
         expect(userName.isDisplayed()).toBe(true);
         userName.sendKeys("13718281997");
-        passWord.sendKeys("ppmoney110");
+        passWord.sendKeys("ppmoney123");
         logIn.click();
-        //expect(loginOk.isDisplayed()).toBe(true);
-        expect(loginWar.valueOf('帐号密码不匹配'));
+        firstPage.click();
+        recharge.click();
+        expect(openAccount.isDisplayed).toBe(true);
+        openAccount.click();
         done();
     });
+
 });
