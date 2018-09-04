468d376b6bffe1c3a52a8b75700e77b5bff2d018
yang-yp <yangyp8084@sina.com>
2018-09-04 22:51:15 +0800

    modify

--- test-framework/pages/letterPage.js
+++ test-framework/pages/letterPage.js
@@ -8,13 +8,14 @@ var LetterPage = function LetterPage() {};
 
 LetterPage.prototype = Object.create(BasePage.prototype);
 LetterPage.prototype.allMsgTable = element(by.partialLinkText('所有消息'));
-LetterPage.prototype.rentMsgTable = element(by.partialLinkText('出借通知'));
+//LetterPage.prototype.rentMsgTable = element(by.partialLinkText('出借通知'));
 LetterPage.prototype.profitMsgTable = element(by.partialLinkText('收益通知'));
 LetterPage.prototype.cashMsgTable = element(by.partialLinkText('提现通知'));
 LetterPage.prototype.inchargeMsgTable = element(by.partialLinkText('充值通知'));
 LetterPage.prototype.sysMsgTable = element(by.partialLinkText('系统通知'));
 LetterPage.prototype.firstPageBtn = element.all(by.css('div#letterlist > div.pp-pagination-wrap > div.pp-pagination > div.pagination > a.item.item-anti ')).first();
 LetterPage.prototype.lastPageBtn = element.all(by.css('div#letterlist > div.pp-pagination-wrap > div.pp-pagination > div.pagination > a.item.item-anti ')).last();
+LetterPage.prototype.rentMsgTable = element(by.css('div.div2.pp-cont-bd > div.div2_t.cf > ul#inMsg-list-hd.ul1 > li > a.current'));
 //LetterPage.prototype.nextPageBtn = element.all(by.css('div#letterlist > div.pp-pagination-wrap > div.pp-pagination > div.pagination > a.item.item-anti ')).next();
 
 module.exports = LetterPage;
